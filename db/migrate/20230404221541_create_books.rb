83335bfdd5bd85fc3a240491aadc281e32efaee2
Cecilia Daninthe <cecilia.daninthe@gmail.com>
2023-04-04 19:14:35 -0400

    adding migrations

--- db/migrate/20230404221541_create_books.rb
+++ db/migrate/20230404221541_create_books.rb
@@ -1,7 +1,13 @@
-class CreateClients < ActiveRecord::Migration[6.1]
+class CreateBooks < ActiveRecord::Migration[6.1]
   def change
-    create_table :clients do |t|
-      t.username :string
+    create_table :books do |t|
+      t.string :title
+      t.integer :pages
+      t.string :review
+      t.boolean :read
+      t.integer :author_id
+      t.integer :genre_id
+      t.timestamps
     end
   end
 end
