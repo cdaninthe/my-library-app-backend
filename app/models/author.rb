b75a1a03daaae594271d0138cf834c68ae60000d
Cecilia Daninthe <cecilia.daninthe@gmail.com>
2023-04-05 11:18:47 -0400

    updated model methods

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,4 +1,11 @@
 class Author < ActiveRecord::Base
     has_many :books
     has_many :genres, through: :books
+
+    # List of books for an author
+    def list_books
+        self.books.map do |book|
+            "#{book.title} - #{book.genre.name}"
+        end
+    end
 end
