1f3da07062ba9252010ff30ead75d3b4c619b437
Cecilia Daninthe <cecilia.daninthe@gmail.com>
2023-04-05 10:57:23 -0400

    model methods and app controller routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,95 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
-  # Add your routes here
-  get "/" do
-    { message: "Good luck with your project!" }.to_json
+  # BOOK ROUTES
+  ### CREATE, UPDATE & DELETE BOOKS
+  get '/books' do
+    Book.all.to_json
+  end
+  
+  get '/books/:id' do
+    book = Book.find(params[:id])
+    book.to_json(include: :author)
+  end
+
+  post '/books' do
+    book = Book.create(
+      title: params[:title],
+      pages: params[:pages],
+      read: false,
+      author_id: params[:author_id],
+      genre_id: params[:genre_id]
+    )
+    book.to_json
+  end
+
+  patch '/books/:id' do
+    book = Book.find(params[:id])
+    book.update(
+      title: params[:title],
+      pages: params[:pages],
+      read: params[:read],
+      author_id: params[:author_id],
+      genre_id: params[:genre_id]
+    )
+    book.to_json
+  end
+
+  delete '/books/:id' do
+    book = Book.find(params[:id])
+    book.destroy
+    book.to_json
+  end
+
+  ### BOOK WITH THE MOST PAGES
+  get '/books_most_pages' do
+    Book.most_pages.to_json
+  end
+
+  ### TOTAL BOOKS IN MY LIBRARY
+  get '/books_total' do
+    Book.total.to_json
+  end
+
+  ### TOTAL BOOKS READ IN MY LIBRARY
+  get '/books_total_read' do
+    Book.total_read.to_json
+  end
+
+  ### UNREAD BOOKS IN MY LIBRARY
+  get '/books_unread' do
+    Book.unread.to_json
+  end
+
+  ### MY NEWEST BOOK (LAST ADDED IN LIBRARY)
+  get '/books_newest' do
+    Book.newest.to_json
+  end  
+
+
+  # AUTHORS ROUTES
+  post '/authors' do
+    author = Author.create(name: params[:name])
+    author.to_json
+  end
+
+  patch '/authors/:id' do
+    author = Author.find(params[:id])
+    author.update(name: params[:name])
+    author.to_json
+  end
+
+
+  # GENRES ROUTES
+  post '/genres' do
+    genre = Genre.create(name: params[:name])
+    genre.to_json
+  end
+
+  patch '/genres/:id' do
+    genre = Genre.find(params[:id])
+    genre.update(name: params[:name])
+    genre.to_json
   end
 
 end
---
+++ app/models/book.rb
@@ -0,0 +1,29 @@
+class Book < ActiveRecord::Base
+    belongs_to :genre
+    belongs_to :author
+
+    # BOOK WITH THE MOST PAGES
+    def self.most_pages
+        self.where('pages =?', self.maximum(:pages))
+    end
+
+    # TOTAL BOOKS IN MY LIBRARY
+    def self.total 
+        self.count
+    end
+
+    # TOTAL BOOKS READ IN MY LIBRARY
+    def self.total_read
+        self.total - self.unread.count
+    end
+
+    # UNREAD BOOKS IN MY LIBRARY
+    def self.unread 
+        self.where('read = false')
+    end
+
+    # MY NEWEST BOOK (LAST ADDED IN LIBRARY)
+    def self.newest
+        self.where('created_at =?', self.maximum(:created_at))
+    end
+end
